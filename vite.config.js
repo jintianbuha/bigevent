4cf8de37ccca97163e01b845b04a792c00ff20c3
jintianbuha <3478977301@qq.com>
2024-08-20 20:35:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   build: {
     outDir: 'dist', // 输出目录
     rollupOptions: {
-      input: './src/main.js' //入口文件
+      input: './index.html' //入口文件
     }
   }
 })
